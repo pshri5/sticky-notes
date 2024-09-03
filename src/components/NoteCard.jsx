39be6c431fde460104afd6138c2be8eaeaefa3d2
Preetam Shri <preetamsshri@gmail.com>
2024-09-03 11:02:38 +0530

    basic setup done

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Trash } from "../icons/trash";
 
 export const NoteCard = ({ note }) => {
     let position = JSON.parse(note.position);
     const colors = JSON.parse(note.colors);
     const body = JSON.parse(note.body);
+    const textAreaRef = useRef(null)
+
+    useEffect(()=>{
+        autoGrow(textAreaRef)
+    },[])
+
+    function autoGrow(textAreaRef) {
+        const { current } = textAreaRef;
+        current.style.height = "auto"; 
+        current.style.height = current.scrollHeight + "px"; 
+    }
  
     return (
         <div
             className="card"
             style={{
                 backgroundColor: colors.colorBody,
+                left: `${position.x}px`,
+                top: `${position.x}py`
             }}
+
         >
             {body}
+            
+            <div className="card-header"
+            style={{color: colors.colorHeader}}>
+              <Trash />  
+
+            </div>
+            
+            
+            
+            <div className="card-body">
+                <textarea 
+                style={{color:colors.colorText}}
+                defaultValue={body}
+                ref={textAreaRef}
+                onInput={() => {
+                    autoGrow(textAreaRef);
+               }}
+                ></textarea>
+            </div>
+
         </div>
     );
 };
--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -3,3 +3,5 @@ import React from "react";
 const NotesPage = () =>{
     return <div>NotesPage</div>
 };
+
+export default NotesPage
